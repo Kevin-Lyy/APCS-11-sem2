9be7c51051890d1d55a93fc10b724f66de1a894a
KevinLyy <batmanthelegendly@gmail.com>
2018-03-05 18:06:06 -0500

    read

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -3,5 +3,42 @@ import java.io,*;
 
 public class Maze{
 
+    private char[][]maze;
+    private boolean animate; //false
+
+    public Maze(String filename) throws FileNotFoundException{
+
+    }
+
+    private void wait(int millis){
+	try{
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e){
+	}
+    }
+
+    public void setAnimate(boolean b){
+	animate = b;
+    }
+
+    public void clearTerminal(){
+	System.out.println("\033[2J\033[1;1H");
+    }
+    public int solve(){
+
+    }
+
+    private int solve(int row, int col){
+
+	if(animate){
+	    clearTerminal();
+	    System.out.println(this);
+	    wait(20);
+	}
+
+	return -1;
+    }
+
 
 }
