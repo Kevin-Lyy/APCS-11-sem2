a64eb5c03a88f2774a3a4461715088aea07d2d1c
KevinLyy <batmanthelegendly@gmail.com>
2018-03-22 21:29:35 -0400

    not merge

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,13 +1,33 @@
 import java.util.*;
 public class  Merge{
 
+    private static void merge(int[] data, int[] temp,int lo,int mid,int hi){
+	int start = lo;
+	int start2 = mid;
+	int l = lo;
+	
+
+    }
+
 
     public static void mergeSort(int[] data){
+	int[] temp = new int[data.length];
+	msort(data,temp,0,data.length-1);
 
     }
 
     public static void msort(int[] data, int[] temp, int lo, int hi){
+	int mid = (lo+hi)/2;
+	if(lo >= h)i{
+	    return data;
+	}
+	msort(temp,data,lo,mid);
+	msort(temp,data,mid+1,hi);
+	merge(data,temp,lo,mid+1,hi);
 
+	for(int c = lo; c < hi; c++){
+	    temp[c] = data[c];
+	}
     }
 }
 
