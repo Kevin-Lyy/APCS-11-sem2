b0cc93bf98fbe20ef9a669be2444ce79f7c63cda
KevinLyy <batmanthelegendly@gmail.com>
2018-03-28 15:34:32 -0400

    nodes

--- 08LinkedList/MyLinkedList.java
+++ 08LinkedList/MyLinkedList.java
@@ -1,3 +1,77 @@
+private class Node{
+    Node next,prev;
+    int data;
+
+    private Node getNext(){
+	return next;
+
+    }
+
+    private void setNext(Node n){
+	next = n;
+	this.next = next;
+
+    }
+
+    private Node getPrev(){
+	return prev;
+
+    }
+
+    private void setPrev(Node p){
+	prev = p;
+	this.prev = prev;
+
+    }
+
+    private int getValue(){
+	return data;
+
+    }
+
+    private void setValue(int d){
+	data = d;
+	this.data = data;
+    }
+
+    private String toString(){
+	while node != null{
+
+
+	}
+
+    }
+    
+
+}
+
 public class MyLinkedList{
+    Node first;
+    Node last;
+    int length;
+
+    public MyLinkedList(){
+	String linkedList = "";
+    }
+
+    public boolean add(int value){
+
+    }
+
+    public int size(){
+
+    }
+
+    public string toString(){
+
+    }
+
+    public int get(int index){
+
+    }
+
+    public int set(int index, int newValue){
+
+    }
 
 }
