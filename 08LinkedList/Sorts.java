76586c6dc4425fafaf8b7faea5fc8d43c3efd44b
KevinLyy <batmanthelegendly@gmail.com>
2018-04-15 17:26:32 -0400

    sorts

--- 08LinkedList/Sorts.java
+++ 08LinkedList/Sorts.java
@@ -4,6 +4,16 @@ Public class Sorts{
     
     public static void radixsort(MyLinkedListImproved<Integer> data){
 	    @SuppressWarnings("unchecked") ArrayList<Integer>[] bucket = new ArrayList[10];
+
+
+	    Integer max = data.get(data.max());
+
+	    for(int c = 0; c < 21;c++){
+		if(c <= 10){
+		    bucket[c] = new ArrayList<Integer>();
+		}
+		else{
+		    negBucket[c-11] = new ArrayList<Integer>();
     }
 
     // public static void negRadixsort(){};
