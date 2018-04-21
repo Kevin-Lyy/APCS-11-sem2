72dadb02eb0ca2ee51023f6c1edc7bb64051c74f
KevinLyy <batmanthelegendly@gmail.com>
2018-04-21 14:49:38 -0400

    untested

--- 08LinkedList/.#Sorts.java
+++
@@ -1,1 +0,0 @@
-kevin@kevin-VirtualBox.2523:1524164656
--- 08LinkedList/Sorts.java
+++ 08LinkedList/Sorts.java
@@ -3,8 +3,10 @@ public class Sorts{
     
     	@SuppressWarnings("unchecked")public static void radixsort(MyLinkedListImproved<Integer> data){
 	    MyLinkedListImproved[] bucket = new MyLinkedListImproved[10];
-	    MyLinkedListImproved[] tempBucket = new MyLinkedListImproved[10];
-	    MyLinkedListImproved[] negBucket = new MyLinkedListImproved[10];	    
+	    MyLinkedListImproved[] nbucket = new MyLinkedListImproved[10];
+	    ArrayList<Integer>[] tempBucket = new ArrayList[10];
+	    ArrayList<Integer>[] negBucket = new ArrayList[10];
+	    ArrayList<Integer>[] shuffle = new ArrayList[10];	    
 	    
 	    if(data.size() <= 1){
 		return ;
@@ -23,93 +25,93 @@ public class Sorts{
 		min = min/10;
 		minDigits++;
 	    }
-
-	    
 	    //creating neg and reg bucket  
 	    for(int c = 0; c <= 10;c++){
-		    bucket[c] = new MyLinkedListImproved<Integer>();
-		    negBucket[c] = new MyLinkedListImproved<Integer>();
+		    tempBucket[c] = new ArrayList<Integer>();
+		    negBucket[c] = new ArrayList<Integer>();
 	    }
 	    
 	    //first sorted bucket, seperates pos and neg
 	    for(int d:data){
 		if(d>0){
-		    bucket[d%10].add(d);
+		    tempBucket[d%10].add(d);
 		}
 		else{
 		    negBucket[-1 * d%10].add(d);
 		}
 	    }
-
-
-
+	    //move values into shuffle
+	    for(int i = 0; i <= 10; i++){
+		for(int d:tempBucket[i]){
+		    shuffle[i].add(d);
+		    tempBucket[i].clear();
+		}
+	    }
+	
 	    //sort pos
-	    for(int c = 2;c <= maxDigits;c++){
-		for(int cont = 0;cont <=10;cont++){
-		    for (int d:bucket){
-			tempBucket[d%(int)Math.pow(10,c)].add(d);
+	    for (int c = 2; c<= maxDigits; c++){
+		for(int cont = 0; cont <= 10; cont++){
+		    for(int d:shuffle[cont]){
+			tempBucket[d% (int)Math.pow(10, (double)c)].add(d);
 		    }
-		}
-	    }
-				   
-				   
-	   
-	    //sort neg
-
-		    
-	    /*broken rn
-	    if(maxDigits > 1){
-		for(int c = 2;c<=maxDigits;c++){
-		    for(int cont = 0; cont <= 10; cont++){
-			for(int d:tempBucket[cont]){
-			    bucket[d%(int)Math.pow(10,(double)c)].add(d);
+		    //clear out bucket to resort
+		    for(int c2 = 0; c2 <= 10; c2++){
+			shuffle[c2].clear();
+			for(int d2:tempBucket[c2]){
+			    shuffle[c2].add(d2);
 			}
-			//clear out bucket
-			for(int c2 = 0;c2 <= 10; c2++){
+			if(c < maxDigits){
 			    tempBucket[c2].clear();
-			    for(int d2:bucket[c2]){			       
-				tempBucket[c2].add(d2);
-			    }
-			    if(c<maxDigits){
-				bucket[c2].clear();
-			    }
 			}
 		    }
 		}
 	    }
-	     
-	    if(minDigits > 1){
-		for(int c = 2;c<=minDigits;c++){
-		    for(int cont = 0; cont <= 10; cont++){
-			for(int d:tempBucket[cont]){
-			    negBucket[-1 * d%(int)Math.pow(10,(double)c)].add(d);
+	    //move neg values into shuffle
+	    for(int i = 0; i <= 10; i++){
+		for(int d:negBucket[i]){
+		    shuffle[i].clear();
+		    shuffle[i].add(d);
+		    negBucket[i].clear();
+		}
+	    }
+	    
+	    for(int c = 2;c<=minDigits;c++){
+		for(int cont = 0; cont <= 10; cont++){
+		    for(int d:shuffle[cont]){
+			negBucket[-1 * d%(int)Math.pow(10,(double)c)].add(d);
+		    }
+		    //clear out bucket
+		    for(int c2 = 0;c2 <= 10; c2++){
+			shuffle[c2].clear();
+			for(int d2:negBucket[c2]){
+			    shuffle[c2].add(d2);
 			}
-			//clear out bucket
-			for(int c2 = 0;c2 <= 10; c2++){
-			    tempBucket[c2].clear();
-			    for(int d2:negBucket[c2]){			       
-				tempBucket[c2].add(d2);
-			    }
-			    if(c<minDigits){
-				negBucket[c2].clear();
-			    }
+			if(c<minDigits){
+			    negBucket[c2].clear();
 			}
 		    }
 		}
 	    }
-	    */
 
-    
+	    for(int i = 0; i <= 10;i++){
+		for(int d:negBucket[i]){
+		    nbucket[i].add(d);
+		}
+		for(int d2:tempBucket[i]){
+		    bucket[i].add(d2);
+		}
+	    }
+	    
 	    //extend
 	    data.clear();
 	    for(int c = 9; c >= 0;c++){
-		data.extend(negBucket[c]);
+		data.extend(nbucket[c]);
 	    }	    
 	    for(int c = 0; c < 10;c++){
 		data.extend(bucket[c]);
 	    }
+  
 	}
-	    
 	    
     public static void radixsortincludingNegatives(MyLinkedListImproved<Integer> data){
 	return;
