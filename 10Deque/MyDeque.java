d784b40241a7b0eafb7b5ea93817833364bc5f38
KevinLyy <batmanthelegendly@gmail.com>
2018-04-22 16:06:28 -0400

    spelling errors are annoying

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,11 +1,11 @@
-import java.util.*
+import java.util.*;
 public class MyDeque<T>{
     private T[] array;
     private int start;
     private int end;
     private int size;
 
-    @SupressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public MyDeque(){
 	array =(T[]) new Object[10];
 	size = 0;
@@ -13,10 +13,10 @@ public class MyDeque<T>{
 	end = 5;
     }
 
-    @SupressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public MyDeque(int initialCapacity){
-	if(initalCapacity < 0){
-	    throw new IllegalArgumentException;
+	if(initialCapacity < 0){
+	    throw new IllegalArgumentException();
 	}
 	array = (T[]) new Object[initialCapacity];
 	size = 0;
@@ -29,41 +29,80 @@ public class MyDeque<T>{
     }
     
     public void addFirst(T value){
-	if(T == null){
-	    throw new NullPointerException;
+	if(value == null){
+	    throw new NullPointerException();
 	}
-
+	if(start == 0){
+	    start = array.length-1;
+	}
+	else{
+	    start--;
+	}
+	array[start] = value;
+	size++;
     }
 
     public void addLast(T value){
-	if(T == null){
-	    throw new NullPointerException;
+	if(value == null){
+	    throw new NullPointerException();
 	}
-
+	if(end == array.length-1){
+	    end = 0;
+	}
+	else{
+	    end++;
+	}
+	array[end] = value;
+	size++;
     }
 
     public T removeFirst(){
 	if (size == 0){
-	    throw new NoSuchElementException;
+	    throw new NoSuchElementException();
 	}
-
+	T first = array[start];
+	array[start] = null;
+	size--;	
+	if(start == array.length){
+	    start = 0;
+	}
+	else{
+	    start++;
+	}   
+	return first;
     }
+	
+	
     public T removeLast(){
 	if (size == 0){
-	    throw new NoSuchElementException;
+	    throw new NoSuchElementException();	    
 	}
-
+	T last = array[end];
+	array[end] = null;
+	size--;
+	if(end == 0){
+	    end = array.length-1;
+	}
+	else{
+	    end--;
+	}
+	return last;
     }
     public T getFirst(){
 	if (size == 0){
-	    throw new NoSuchElementException;
+	    throw new NoSuchElementException();
 	}
-	
+	T first = array[start];	
+	return first;	
     }
+    
     public T getLast(){
 	if (size == 0){
-	    throw new NoSuchElementException;
+	    throw new NoSuchElementException();
 	}
-    }   
+	T last = array[end];
+	return last;
+    }
+    
 }
 
