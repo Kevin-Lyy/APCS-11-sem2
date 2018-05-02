c6b5d9a69bc8dd229e07b346e286e83b7797c421
unknown <batmanthelegendly@gmail.com>
2018-05-02 15:10:20 -0400

    startofsort

--- 12MyHeap/Sorts.java
+++ 12MyHeap/Sorts.java
@@ -1,7 +1,14 @@
-public class Sorts{
+import java.util.*;
 
-	private static HeapSort{
+public class Sorts<T extends Comparable<T>>{
 
+	private static HeapSort(Integer[] heap){
+		MyHeap<Integer> sortHeap = new MyHeap<>();
+		for(int startOfSort = heap.length; startOfSort >= 0; startOfSort--) {
+			sortHeap.add(heap[startOfSort]);
+		}
+		while(startOfSort > 0) {
+			data.remove(startOfSort);
+		}
 	}
-
 }
