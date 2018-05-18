547b2cb768291ab263c4be723286798d1b403b49
unknown <batmanthelegendly@gmail.com>
2018-05-17 22:32:15 -0400

    quickly merge

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,34 +1,36 @@
 import java.util.*;
 public class  Merge{
 
-  private static void merge(int[] data, int[] temp,int lo,int end1, int start2, int hi){
-	int tStart = lo;
-	while(lo < end1 && start2 <= hi){
-	    if(temp[lo] < temp[start2]){
-		      data[tStart] = temp[lo];
+    private static void merge(int[] data, int[] temp, int lo, int mid, int midUp, int hi){
+      int tStart = lo;
+
+      while (lo <= mid && midUp <= hi){
+        if (temp[lo] < temp[midUp]){
+          data[tStart] = temp[lo];
           lo++;
-	    }
-	    else{
-		      data[tStart] = temp[start2];
-          start2++;
-	    }
+        }
+        else {
+          data[tStart] = temp[midUp];
+          midUp++;
+        }
         tStart++;
-	}
-	while(lo <= end1){
-	    data[tStart] = temp[lo];
-	    lo++;
-	    tStart++;
-	}
-	while(start2 <= hi){
-	    data[tStart] = temp[start2];
-	    start2++;
-	    tStart++;
-	}
+        }
 
-    }
+        while (lo <= mid){
+          data[tStart] = temp[lo];
+          tStart++;
+          lo++;
+        }
+
+        while (midUp <= hi){
+          data[tStart] = temp[midUp];
+          tStart++;
+          midUp++;
+        }
+      }
 
 
-  public static void mergeSort(int[] data){
+  public static void mergesort(int[] data){
 	   int[] temp = new int[data.length];
 	    msort(data,temp,0,data.length-1);
     }
@@ -41,12 +43,27 @@ public class  Merge{
       temp[c] = data[c];
     }
 
-	int mid = (lo+hi)/2;
-	msort(temp,data,lo,mid);
-	msort(temp,data,mid+1,hi);
-	merge(data,temp,lo,mid,mid+1,hi);
+	   int mid = (lo+hi)/2;
+	    msort(temp,data,lo,mid);
+	    msort(temp,data,mid+1,hi);
+	    merge(data,temp,lo,mid,mid+1,hi);
 
 
     }
+
+    public static String toString(int[] data){
+  String ans = "[";
+  for (int x = 0; x < data.length; x++){
+   if (x == data.length - 1){
+ ans = ans + data[x] + "]";
+   }
+   else {
+ ans = ans + data[x] + ", ";
+   }
+
+ }
+  return ans;
+  }
+
 
 }
