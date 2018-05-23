11fbaea0d2172abf2e5f639146d5768be4cfc5a8
unknown <batmanthelegendly@gmail.com>
2018-05-23 19:41:13 -0400

    remove main

--- 12MyHeap/Sorts.java
+++ 12MyHeap/Sorts.java
@@ -12,31 +12,4 @@ public class Sorts extends MyHeap{
 		}
 	}
 
-	public static void main(String[] args){
- 	int[] d1 = new int[10];
- 	int[] d2 = new int[10];
- 	for (int i = 0; i < 10; i++){
- 	    int num = (int)(Math.random() * 1000);
- 	    d1[i] = num;
- 	    d2[i] = num;
- 	}
-
- 	System.out.println(Arrays.toString(d1));
- 	System.out.println(Arrays.toString(d2));
-
- 	Arrays.sort(d1);
- 	heapsort(d2);
-
- 	for (int i = 0; i < 10; i++){
- 	    if (d1[i] != d2[i]){
- 		System.out.println("error at "+ i);
- 		System.out.println(Arrays.toString(d1));
- 		System.out.println(Arrays.toString(d2));
- 		System.exit(1);
- 	    }
- 	}
-
- 	System.out.println("no error");
-     }
-
 }
