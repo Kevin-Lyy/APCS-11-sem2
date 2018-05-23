d08b98b164bf287bbe5e6c6f3fc9dca91b16d571
unknown <batmanthelegendly@gmail.com>
2018-05-23 19:37:04 -0400

    donedonedonedonedone

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -1,10 +1,10 @@
 import java.util.*;
+@SuppressWarnings("unchecked")
 public class RunningMedian extends MyHeap{
 
-	private double currentMedian;
-	MyHeap<Double> lrg;
-	MyHeap<Double> sml;
-	public int size;
+	private MyHeap<Double> lrg;
+	private MyHeap<Double> sml;
+	private int size;
 
 	public RunningMedian(){
 		lrg = new MyHeap<Double>(false);
@@ -16,21 +16,19 @@ public class RunningMedian extends MyHeap{
 		return size;
 	}
 
-	public void add(Double x) {
-		if(size() == 0 || x > sml.peek() == 0) lrg.add(x);
+	public void add(double x){
+		if (size() == 0 || x > lrg.peek()) lrg.add(x);
 		else sml.add(x);
-
-		if(lrg.size() > sml.size())currentMedian = lrg.peek();
-		else if(lrg.size() > sml.size()+1) sml.add(lrg.remove());
-		if(lrg.size < sml.size)currentMedian = sml.peek();
-		else if(sml.size() > lrg.size()+1) lrg.add(sml.remove());
-
+		if (lrg.size() - sml.size() > 1) sml.add(lrg.remove());
+		if (sml.size() - lrg.size() > 1) lrg.add(sml.remove());
 		size++;
 	}
 
 	public double getMedian() {
-		if(size == 0) throw new NoSuchElementException() ;
-		return currentMedian;
+		if(size() == 0) throw new NoSuchElementException() ;
+		if(sml.size() > lrg.size()) return sml.peek();
+		if(lrg.size() > sml.size()) return lrg.peek();
+		return (sml.peek() + lrg.peek()) / 2.0;
 	}
 
 }
