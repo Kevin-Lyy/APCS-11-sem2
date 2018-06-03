14e1a8bc13f70c6f7b62f7675b6c783974987be8
unknown <batmanthelegendly@gmail.com>
2018-06-03 00:44:02 -0400

    unsolved

--- 14Frontier/Location.java
+++ 14Frontier/Location.java
@@ -1,3 +1,38 @@
 Public class Location impletments Comparable<Location>{
+  private int x, y, dist, priority ;
+  Location prev;
+  boolean aStar;
+
+  public Location(int _x,int _y, Location prevs,int dists, boolean aStarq){
+    x = _x;
+    y = _y;
+    prev = prevs;
+    dist = dists;
+    aStar = aStarq;
+  }
   
+  public int getX(){
+    return x;
+  }
+
+  public int getY(){
+    return y;
+  }
+
+  public Location getPrev(){
+    return prev;
+  }
+
+  public String to String(){
+    return x + " " + y;
+  }
+
+  public int getPriority(){
+    return priority;
+  }
+
+  public int getDist(){
+    return dist;
+  }
+
 }
